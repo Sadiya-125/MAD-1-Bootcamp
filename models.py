7fb985ab81d6ca51460c9e241571d816f9297d1d
Sadiya Maheen Siddiqui <wajid0257@outlook.com>
2026-07-17 19:58:54 +0530

    Database and Models Creation

---
+++ models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class Section(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    products = db.relationship('Products', backref='section', cascade="all, delete", lazy=True)
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False) 
+    units = db.Column(db.String(20))
+    stock = db.Column(db.Integer, default=0)
+    section_id = db.Column(db.Integer, db.ForeignKey('section.id'), nullable=False)
