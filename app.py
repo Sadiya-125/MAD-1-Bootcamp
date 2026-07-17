482a5057f6ad3849ba768c9cc6b4ed686cce38e9
Sadiya Maheen Siddiqui <wajid0257@outlook.com>
2026-07-17 19:59:38 +0530

    Database and Models Creation

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from models import db, Section
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///freshcart.db'
+app.config['SECRET_KEY'] = 'freshcart-secret'
+db.init_app(app)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    sections = Section.query.all()
+    return render_template('home.html', sections=sections)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all() # instance/freshcart.db
+    app.run(debug=True, port=8080)
